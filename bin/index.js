2f1e4dd3df6837ff216e71e58cf2989a5e75ee41
yyz945947732 <945947732@qq.com>
2025-05-16 10:31:30 +0800

    style: code lint

--- bin/index.js
+++ bin/index.js
@@ -1,23 +1,23 @@
 #!/usr/bin/env node
 
-import fs from "fs-extra";
-import updater from "update-notifier";
-import parseArgs from "yargs-parser";
+import fs from 'fs-extra';
+import updater from 'update-notifier';
+import parseArgs from 'yargs-parser';
 
-import cli from "../lib/index.js";
+import cli from '../lib/index.js';
 
 const pkg = JSON.parse(
-  fs.readFileSync(new URL("../package.json", import.meta.url), "utf8")
+  fs.readFileSync(new URL('../package.json', import.meta.url), 'utf8')
 );
 
 const aliases = {
-  h: "help",
-  v: "version"
+  h: 'help',
+  v: 'version',
 };
 
-const parseCliArguments = args => {
+const parseCliArguments = (args) => {
   const options = parseArgs(args, {
-    alias: aliases
+    alias: aliases,
   });
   return options;
 };
@@ -26,7 +26,6 @@ const options = parseCliArguments([].slice.call(process.argv, 2));
 
 updater({ pkg }).notify();
 
-cli(options).then(
-  () => {},
-  ({ code }) => process.exit(Number.isInteger(code) ? code : 1)
+cli(options).catch(({ code }) =>
+  process.exit(Number.isInteger(code) ? code : 1)
 );
